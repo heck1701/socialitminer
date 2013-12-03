83f62a82f3a75f6b67c60faaf161afe4a9f692cc
Andrew Shaw <zeckal4@gmail.com>
2013-12-03 05:00:45 -0700

    basics for login page added

--- login.php
+++ login.php
@@ -1,3 +1,18 @@
 <?php include("header.php"); ?>
-<?php echo "Welcome to Login page"; ?>
+
+	<p style="font-size:xx-large;">Login</p> 
+	<form action="LoginProcess.php" method = "post">
+		<table border = "0">
+			<tr>
+				<td>Username:</td>
+				<td align="center"><input type="text" name="UserName" size="40"/> </td>
+			</tr>
+			<tr>
+				<td>Password:</td>
+				<td align="center"><input type="text" name="Password" size="40"/> </td>		<!-- TODO add security for the login process -->
+			</tr>
+		</table>
+		<input style="width:20em;" type="submit" value="Login"/>
+	</form>
+	
 <?php include("footer.php"); ?>
