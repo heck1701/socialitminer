783ef9cb6e23be5acd0374e556b5328314aadd89
Andrew Shaw <zeckal4@gmail.com>
2013-12-03 03:58:16 -0700

    Cant get POST or any mysql working, otherwise Search pages are ready to be linked up

--- search.php
+++ search.php
@@ -1,3 +1,19 @@
 <?php include("header.php"); ?>
-<?php echo "Welcome to Search page"; ?>
+
+	<p style="font-size:xx-large;">Search</p> 
+	<form action="processSearch.php" method = "post">
+		<table border = "0">
+			<tr>
+				<td>Title:</td>
+				<td align="center"><input type="text" name="titlesearch" id="titlesearch" size="40"/> </td>
+			</tr>
+			<tr>
+				<td>User:</td>
+				<td align="center"><input type="text" name="usersearch" size="40"/> </td>
+			</tr>
+		</table>
+		<input style="width:20em;" type="submit" value="Search"/>
+	</form>
+
+
 <?php include("footer.php"); ?>
