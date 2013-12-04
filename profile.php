afb85926f453d9b24f981f1031160f09f662e211
Martin Heck <mheck+ghneghvar@mines.edu>
2013-12-03 17:03:44 -0700

    Fixing profile page while logged out

--- profile.php
+++ profile.php
@@ -1,24 +1,29 @@
 <?php
 include("header.php");
+if(isset($personMarkup)) {
 
-echo "<p style='font-size:xx-large;text-transform:uppercase;'>$dispname</p>";
-	
-$query = sprintf("SELECT RequestID, Title, Body, Votes FROM Requests WHERE OwnerID='$username';");
-if($results = mysqli_query($sqllink,$query)) {
-	echo '<table class="reqlist">';
-	while($row = mysqli_fetch_assoc($results)){
-		echo '<tr>';
-		echo '<td style="font-size:x-large;">'.$row['Title'].'</td>'; // TODO - Click on the title to go to the full post
-		echo '<td style="text-align: right;">Votes:&nbsp;'.$row['Votes'].'</td>';
-		echo '</tr>';
-		echo '<tr>';
-		echo '<td colspan="2">'.$row['Body'].'</td>';
-		echo '</tr>';
+	echo "<p style='font-size:xx-large;text-transform:uppercase;'>$dispname</p>";
+		
+	$query = sprintf("SELECT RequestID, Title, Body, Votes FROM Requests WHERE OwnerID='$username';");
+	if($results = mysqli_query($sqllink,$query)) {
+		echo '<table class="reqlist">';
+		while($row = mysqli_fetch_assoc($results)){
+			echo '<tr>';
+			echo '<td style="font-size:x-large;">'.$row['Title'].'</td>'; // TODO - Click on the title to go to the full post
+			echo '<td style="text-align: right;">Votes:&nbsp;'.$row['Votes'].'</td>';
+			echo '</tr>';
+			echo '<tr>';
+			echo '<td colspan="2">'.$row['Body'].'</td>';
+			echo '</tr>';
+		}
+		echo '</table>';
 	}
-	echo '</table>';
-}
+	else {
+		echo 'No requests';
+	}
 else {
-	echo 'No requests';
+	echo "Not logged in!";
 }
+
 include("footer.php");
 ?>
